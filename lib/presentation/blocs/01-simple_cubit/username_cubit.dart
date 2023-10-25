1195b4828c07410b2669a1cebab062cdc7d3a38d
Khristopher Santiago <khrsantiago@gmail.com>
2023-10-25 14:58:31 -0400

    cubit test

---
+++ lib/presentation/blocs/01-simple_cubit/username_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class UsernameCubit extends Cubit<String> {
+  // UsernameCubit(super.initialState);
+  UsernameCubit() : super('no-username');
+
+
+}
